1a3b9d65b82007f05bcb2bd6786607e09094470a
ZHOUGR2 <286348794@qq.com>
2020-08-06 12:24:17 +0800

    [todos-server] [Green] feature  :implement get all todos

--- src/main/java/com/oocl/todos/controller/ToDosController.java
+++ src/main/java/com/oocl/todos/controller/ToDosController.java
@@ -1,14 +1,24 @@
 package com.oocl.todos.controller;
 
+import com.oocl.todos.dto.ToDoResponse;
+import com.oocl.todos.service.ToDoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/todos")
 public class ToDosController {
+    @Autowired
+    private ToDoService toDoService;
     @GetMapping
-    public String index(){
-        return "hello";
+    @ResponseStatus(HttpStatus.OK)
+    public List<ToDoResponse> getAll(){
+       return toDoService.getAll();
     }
 }
